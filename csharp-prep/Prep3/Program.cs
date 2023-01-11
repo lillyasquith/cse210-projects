a6a390976d9fcb3742ab10ee86d92e3ce66d8d1d
lillyasquith <lydang26993@gmail.com>
2023-01-11 08:44:04 -0700

    Learning Activity - C# Prep 3 Overview

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        Console.WriteLine("Welcome to the Guess My Number game!");
+
+        Console.Write("What is the magic number? ");
+        int number = randomGenerator.Next(1, 10);
+        Console.WriteLine($"{number}");
+
+        Console.Write("What is your guess? ");
+        string valueFromUser = Console.ReadLine();
+        int inputInNumber = int.Parse(valueFromUser);
+
+        if (inputInNumber > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (inputInNumber < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+
     }
 }
