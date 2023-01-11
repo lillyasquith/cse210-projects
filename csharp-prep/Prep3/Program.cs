0f46996039d4f5f0e1028636c637621972502ef8
lillyasquith <lydang26993@gmail.com>
2023-01-11 16:51:44 -0700

    Learning Activity - C# Prep 3 (adjusted code for all 3 core requirements)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,30 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-
         Console.WriteLine("Welcome to the Guess My Number game!");
 
         Console.Write("What is the magic number? ");
-        int number = randomGenerator.Next(1, 10);
-        Console.WriteLine($"{number}");
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine($"{magicNumber}");
 
-        Console.Write("What is your guess? ");
-        string valueFromUser = Console.ReadLine();
-        int inputInNumber = int.Parse(valueFromUser);
+        int input = 0;
+        while (input != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string valueFromUser = Console.ReadLine();
+            int valueInNumber = int.Parse(valueFromUser);
 
-        if (inputInNumber > number)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if (inputInNumber < number)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+            if (valueInNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (valueInNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
 
+        }
 
     }
 }
