7fd6b2f873dd71ace11db03df28b2b8a40ff4024
lillyasquith <lydang26993@gmail.com>
2023-03-27 15:37:31 -0600

    delete test comments in Main

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,35 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        //test-------------------------------------------------------------
-        // Product product1 = new Product("Skinny Jean", "0024SJ", 45, 2);
-        // product1.DisplayProduct();
-        // Console.WriteLine(product1.ProductFee());
-        // Address address1 = new Address("100 N 100 E", "Provo", "UT", "USA");
-        // Customer customer1 = new Customer("Tony Goodman", address1);
-        // customer1.DisplayCustomer();
-        // Order order = new Order(customer1);
-        // order.AddProduct(product1);
-        // Console.WriteLine(order.ShippingCost());
-        // Console.WriteLine(order.OrderTotal());
-
-        //test---------------------------------------------------------------
-        // Product product2 = new Product("Blue Sweatshirt", "0003BS", 35, 1);
-        // product2.DisplayProduct();
-        // Product product3 = new Product("Bomber Jacket", "001XZY", 180, 1);
-        // product3.DisplayProduct();
-        // Console.WriteLine(product2.ProductFee());
-        // Console.WriteLine(product3.ProductFee());
-        // Address address2 = new Address("123 Main Street", "Montreal", "QC", "Canada");
-        // Customer customer2 = new Customer("John White", address2);
-        // customer2.DisplayCustomer();
-        // Order order2 = new Order(customer2);
-        // order2.AddProduct(product2);
-        // order2.AddProduct(product3);
-        // Console.WriteLine(order2.ShippingCost());
-        // // //need to add a product to a list
-        // Console.WriteLine(order2.OrderTotal());
-
         Product product1 = new Product("Skinny Jean", "0024SJ", 45, 2);
         Address address1 = new Address("100 N 100 E", "Provo", "UT", "USA");
         Customer customer1 = new Customer("Tony Goodman", address1);
